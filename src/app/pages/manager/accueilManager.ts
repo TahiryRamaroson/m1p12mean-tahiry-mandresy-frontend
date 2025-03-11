1eb03dbdd954edd59a0804d2c55d1aa5e53a3bbe
TahiryHenitsoa <118928190+TahiryRamaroson@users.noreply.github.com>
2025-03-11 18:13:18 +0300

    Préparation structure affichage

--- src/app/pages/manager/accueilManager.ts
+++ src/app/pages/manager/accueilManager.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-empty',
     standalone: true,
     template: ` <div class="card">
-        <div class="font-semibold text-xl mb-4">Empty Page</div>
+        <div class="font-semibold text-xl mb-4">Accueil manager</div>
         <p>Use this page to start from scratch and place your custom content.</p>
     </div>`
 })
-export class Empty {}
+export class AccueilManager {}
