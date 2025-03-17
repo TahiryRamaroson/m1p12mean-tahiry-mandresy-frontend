311e1a9fe775eda4aa1322688857290e12161a94
TahiryHenitsoa <118928190+TahiryRamaroson@users.noreply.github.com>
2025-03-17 15:30:47 +0300

    création page demande de rendez-vous client

--- src/app/pages/client/demande-rendez-vous-client/demande-rendez-vous-client.component.ts
+++ src/app/pages/client/demande-rendez-vous-client/demande-rendez-vous-client.component.ts
@@ -2,7 +2,11 @@ import { Component, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { FormsModule } from '@angular/forms';
-
+import { DialogModule } from 'primeng/dialog';
+import { CalendarOptions } from '@fullcalendar/core'; 
+import { FullCalendarModule } from '@fullcalendar/angular';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
 
 @Component({
   selector: 'app-demande-rendez-vous-client',
@@ -11,6 +15,8 @@ import { FormsModule } from '@angular/forms';
     CommonModule,
     ButtonModule,
     FormsModule,
+    FullCalendarModule,
+    DialogModule
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './demande-rendez-vous-client.component.html',
@@ -18,7 +24,32 @@ import { FormsModule } from '@angular/forms';
 })
 
 export class DemandeRendezVousClientComponent {
-  envoyerDemande() {
-      console.log(' date.');
+  calendarOptions: CalendarOptions = {
+    plugins: [dayGridPlugin, interactionPlugin],
+    initialView: 'dayGridMonth',
+    selectable: true,
+    editable: true,
+    dateClick: this.handleDateClick.bind(this) // Bind the method to the component context
+  };
+
+  displayModal: boolean = false;
+  selectedDate: Date | null = null;
+
+  handleDateClick(arg: any) {
+    this.selectedDate = arg.date;
+    this.displayModal = true;
+  }
+
+  hideModal() {
+    this.displayModal = false;
+  }
+
+  confirmerDemande() {
+    if (this.selectedDate) {
+      console.log('Demande de rendez-vous envoyée pour la date :', this.selectedDate);
+      this.hideModal();
+    } else {
+      console.log('Veuillez sélectionner une date.');
+    }
   }
 }
